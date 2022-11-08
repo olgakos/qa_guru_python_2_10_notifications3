ecba68302c4a0977a7156603f03fca476c066329
Olga <ok2304@yandex.ru>
2022-11-08 14:13:43 +0300

    fix

--- tests/test_github.py
+++ tests/test_github.py
@@ -5,7 +5,7 @@ from selene.support.conditions import be
 from selene.support.shared import browser
 from selene.support.shared.jquery_style import s
 
-
+'''
 #запускается, но
 #selenium.common.exceptions.WebDriverException: Message: 'chromedriver' executable needs to be in PATH. Please see https://chromedriver.chromium.org/home
 @pytest.fixture(scope="session")
@@ -13,10 +13,11 @@ def browser_open_setting():
     browser.config.window_width = 1400 #NB!
     browser.config.window_height = 600
     yield
-    
+'''
 
-def test_github(browser_open_setting):
+#def test_github(browser_open_setting):
 #def test_github():
+def test_github(setup_browser):
     with allure.step("Open Home Page"):
         browser.open("https://github.com")
     with allure.step("Search"):
